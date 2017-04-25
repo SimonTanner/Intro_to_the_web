6c886a43a52c1822bcdf684fc680603a18483464
SimonTanner <simonthetanner@hotmail.com>
2017-04-25 12:25:48 +0000

    Cat form page added and post method used instead of get

--- app.rb
+++ app.rb
@@ -23,8 +23,13 @@ get '/random-cat' do
     erb(:index)
 end
 
-get '/named-cat' do
+get'/cat-form' do
+    erb(:index_2)
+end
+
+post '/named-cat' do
     p params
     @name = params[:name]
     erb(:index)
 end
+
